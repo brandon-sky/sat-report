c7c3e1ac54aa66e0fe3b08c737588ed2aece92ba
brandon <brandon@onlinehome.de>
2024-02-14 15:39:58 +0000

    feat: implement momentum

--- src/app.py
+++ src/app.py
@@ -4,17 +4,26 @@ import pandas as pd
 import streamlit as st 
 
 from data.gamelogs import get_gamelog_path_map
-from bi.sidebar import display_sidebar
+from bi.sidebar import display_sidebar, display_momentum
 
 
 def main():
     path_map = get_gamelog_path_map()
     dataframe = display_sidebar(path_map)
-    
-    st.header("Hello")
-    with st.expander(label="Dataset"):
-        if not dataframe is None:
-            st.dataframe(dataframe)
+        
+    if not dataframe is None:
+
+        sorted_data = dataframe .sort_values(by=["QTR", "SERIES", "PLAY #"])
+        sorted_data.reset_index(drop=True, inplace=True)
+
+
+        
+        st.header("Hello")
+        with st.expander(label="Dataset"):
+                st.dataframe(sorted_data)
+        
+        with st.expander(label="Momentum"):
+                display_momentum(sorted_data)
     return 
 
 if __name__ == "__main__":
